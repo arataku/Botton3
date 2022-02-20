17414193a22255a4c1b79e254c9e42bd821ae332
arataku <44110164+arataku@users.noreply.github.com>
2022-02-20 11:53:59 +0900

    Revert "change the setting of gitignore"
    This reverts commit 8c69cc51c084eced1cd9ded6ccb9960ed3c7a133.

--- build/Utls.js
+++ build/Utls.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = exports.average = void 0;
+exports.average = void 0;
 function average(array) {
     let sum = 0;
     for (let value of array) {
@@ -9,8 +9,4 @@ function average(array) {
     return sum / array.length;
 }
 exports.average = average;
-async function sleep(waitTime) {
-    return new Promise((resolve) => setTimeout(resolve, waitTime));
-}
-exports.sleep = sleep;
 //# sourceMappingURL=Utls.js.map
